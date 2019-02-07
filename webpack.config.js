21578653636a61fc75fbc1c8e8f82ca43d8e9163
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2019-02-07 17:02:09 -0500

    Add the last missing files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ if (minimize) {
   console.log('Building non minified version of the library'.bgGreen.red);
 }
 
-const rootFolder = "./bin";
+const rootFolder = "./docs";
 
 const entries = {};
 components.getComponentNames().forEach(componentName => {
