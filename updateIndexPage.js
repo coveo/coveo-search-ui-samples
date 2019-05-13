b22503c5fe5bcdf55526293bd2c2ffd33e30bc6b
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2019-05-13 10:18:33 -0400

    Update docs files following JSUI bump, fix issues in updateIndexPage.js

--- updateIndexPage.js
+++ updateIndexPage.js
@@ -3,7 +3,7 @@ const components = require("./components");
 
 const rootFolder = require("./config").rootPath;
 
-const getReadmeLinkForComponentName = (componentName) => `https://github.com/coveo/coveo-search-ui-samples/blob/master/components/${componentName}/readme.md"`;
+const getReadmeLinkForComponentName = (componentName) => `https://github.com/coveo/coveo-search-ui-samples/blob/master/components/${componentName}/readme.md`;
 const getElementLinkForComponentName = (componentName) => `<a href="${componentName}.html">${componentName}</a> (<a href="${getReadmeLinkForComponentName(componentName)}">readme</a>)`;
 
 const links = components.getComponentNames()
@@ -21,7 +21,7 @@ const page = `<!DOCTYPE html>
   </head>
   <body>
     <h2>${title}</h2>
-    <ul>${links.join("")}</ul>
+    <ul>${links.join("\n")}</ul>
   </body>
 </html>
 `;
